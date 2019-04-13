ed44d233043bdc7949ef1807a26b433e73f462a2
aolo2 <aolo2@yandex.ru>
2019-04-13 21:23:24 +0300

    8

--- main.c
+++ main.c
@@ -2,25 +2,24 @@
 
 #include "inst.c"
 #include "cfg.c"
+#include "ir.c"
 #include "ssa.c"
 #include "opt.c"
-#include "ir.c"
 
 s32
 main(void)
 {
     u32 num_words;
-    u32 *words = get_binary("data/ssa.frag.spv", &num_words);
+    u32 *words = get_binary("data/cycle.frag.spv", &num_words);
     
     struct ir file = eat_ir(words, num_words);
     free(words);
     
-    u32 ver[] = { 0, 1, 2 };
+    //struct cfg_dfs_result dfs = cfg_dfs(&file.cfg);
+    //struct uint_vector df = ssa_dominance_frontier(&file.cfg, &dfs, ver, 2);
+    ssa_convert(&file);
     
-    struct cfg_dfs_result dfs = cfg_dfs(&file.cfg);
-    struct uint_vector df = ssa_dominance_frontier(&file.cfg, &dfs, ver, 3);
-    
-    dump_ir(&file, "data/ssa2.frag.spv");
+    dump_ir(&file, "data/cycle2.frag.spv");
     destroy_ir(&file);
     
     return(0);
--- ssa.c
+++ ssa.c
@@ -110,3 +110,69 @@ ssa_dominance_frontier(struct ir_cfg *cfg, struct cfg_dfs_result *dfs, u32 *vert
     
     return(dfp);
 }
+
+static void
+ssa_convert(struct ir *file)
+{
+    struct uint_vector variables = vector_init();
+    
+    for (u32 i = 0; i < file->cfg.labels.size; ++i) {
+        struct basic_block *block = file->blocks + i;
+        struct instruction_list *instruction = block->instructions;
+        bool reading = false;
+        
+        while (instruction) {
+            if (instruction->data.opcode == OpVariable) {
+                reading = true;
+                vector_push(&variables, instruction->data.OpVariable.result_id);
+            } else if (reading) {
+                // NOTE: we've read all OpVariables. 
+                // This is based on the SPIR-V specification!
+                break;
+            }
+            instruction = instruction->next;
+        }
+    }
+    
+    struct uint_vector *store_blocks = malloc(variables.size * sizeof(struct uint_vector));
+    for (u32 i = 0; i < variables.size; ++i) {
+        store_blocks[i] = vector_init();
+    }
+    
+    for (u32 block_index = 0; block_index < file->cfg.labels.size; ++block_index) {
+        struct basic_block *block = file->blocks + block_index;
+        struct instruction_list *instruction = block->instructions;
+        
+        while (instruction) {
+            if (instruction->data.opcode == OpStore) {
+                u32 store_to = search_item_u32(variables.data, variables.size, 
+                                               instruction->data.OpStore.pointer);
+                vector_push(store_blocks + store_to, block_index);
+            }
+            instruction = instruction->next;
+        }
+    }
+    
+    int a = 1;
+    
+    // TODO:
+    // pass all opstores to compute DF
+    // for each node in DF we need to find where control came from
+    // WE KNOW that it either directly from one of the opstore nodes or 
+    // there is a straight path from opstore node to us (check all :( )
+    // we know that because the predecessor is dominated by one of the blocks
+    // we need only to know which of the predecessors is dominated by which block
+    // do it by checking all :-)
+    
+    // insert phi functions
+    // pseudocode, we have DF and parent_nodes
+    // for (node of DF)
+    //     phi = {}
+    //     for (predecessor of node)
+    //         for (parent of parent_nodes)
+    //             if (dominates(parent, predecessor)
+    //                  phi.add({predecessor, value in parent })
+    
+    // reindex
+    
+}
